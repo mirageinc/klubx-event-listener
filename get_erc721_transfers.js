fdebd670f2d78414cbfb282651f5be38ab86f44d
thallard <thallard@student.42lyon.fr>
2022-11-04 15:42:05 +0100

    Get ERC721 transfers or cryptopunks transfers

---
+++ get_erc721_transfers.js
@@ -0,0 +1,40 @@
+import postgres from "postgres";
+import "dotenv/config";
+
+const sql = postgres(process.env.DATABASE_URI, {
+  transform: postgres.camel,
+});
+
+export async function get_erc721_transfers(contract_address, token_id) {
+  let res = null;
+
+  contract_address = contract_address.toLowerCase();
+  const tokenId_hex = token_id.toString(16);
+  const data = "0x" + tokenId_hex.padStart(67 - tokenId_hex.length, "0");
+
+  contract_address = contract_address.toLowerCase();
+
+  if (contract_address == "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb")
+    res = await sql`select transaction_hash, 
+    concat('0x', substring(topics[2] from 27)) AS sender,
+    concat('0x', substring(topics[3] from 27)) AS receiver,
+    ethereum.hex_to_int(ethereum.skip_zeros(data)) AS token_id
+    from   ethereum.log
+    where  contract_address = ${contract_address} and
+    topics[1] = '0x05af636b70da6819000c49f85b21fa82081c632069bb626f30932034099107d8' and
+    data = ${data};`;
+  else
+    res = await sql`select transaction_hash, 
+    concat('0x', substring(topics[2] from 27)) AS sender,
+    concat('0x', substring(topics[3] from 27)) AS receiver,
+    ethereum.hex_to_int(ethereum.skip_zeros(topics[4])) AS token_id
+    from   ethereum.log
+    where  contract_address = ${contract_address} and
+    topics[4] = ${data};`;
+
+  return res;
+}
+
+console.log(
+  await get_erc721_transfers("0x942bc2d3e7a589fe5bd4a5c6ef9727dfd82f5c8a", 7551)
+);
